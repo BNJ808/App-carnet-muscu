92296e15e40c6616cea37d923b57edaa2f8184a6
BNJ808 <benji.tess@gmail.com>
2025-06-10 01:36:56 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-// main.jsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import MinimalApp from './MinimalApp.jsx'; // Assurez-vous que le chemin est correct
+import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <MinimalApp />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
